d3cd248cc00fbfa2d42bdb68fd84bfc78ab3a53f
Kirill <vek7434625@gmail.com>
2024-07-30 16:18:40 +0500

    Fix models

--- building/buildings/migrations/0001_initial.py
+++ building/buildings/migrations/0001_initial.py
@@ -9,42 +9,116 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Apartments',
+            name="Apartments",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('area', models.DecimalField(decimal_places=2, max_digits=5, verbose_name='Area')),
-                ('rooms', models.CharField(choices=[('1', 'One'), ('2', 'Two'), ('3', 'Three'), ('4+', 'Four Plus'), ('Студия', 'Studio')], default='1', max_length=20, verbose_name='Rooms')),
-                ('price', models.DecimalField(decimal_places=2, max_digits=19, verbose_name='Price')),
-                ('floor', models.PositiveIntegerField(verbose_name='Floor')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "area",
+                    models.DecimalField(
+                        decimal_places=2, max_digits=5, verbose_name="Area"
+                    ),
+                ),
+                (
+                    "rooms",
+                    models.CharField(
+                        choices=[
+                            ("1", "One"),
+                            ("2", "Two"),
+                            ("3", "Three"),
+                            ("4+", "Four Plus"),
+                            ("Студия", "Studio"),
+                        ],
+                        default="1",
+                        max_length=20,
+                        verbose_name="Rooms",
+                    ),
+                ),
+                (
+                    "price",
+                    models.DecimalField(
+                        decimal_places=2, max_digits=19, verbose_name="Price"
+                    ),
+                ),
+                ("floor", models.PositiveIntegerField(verbose_name="Floor")),
             ],
         ),
         migrations.CreateModel(
-            name='Building',
+            name="Building",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(blank=True, max_length=100, verbose_name='Name')),
-                ('year', models.IntegerField(verbose_name='Year')),
-                ('city', models.CharField(max_length=100, verbose_name='City')),
-                ('street', models.CharField(max_length=100, verbose_name='Street')),
-                ('number', models.IntegerField(verbose_name='Number')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "name",
+                    models.CharField(
+                        blank=True, max_length=100, verbose_name="Name"
+                    ),
+                ),
+                ("year", models.IntegerField(verbose_name="Year")),
+                ("city", models.CharField(max_length=100, verbose_name="City")),
+                (
+                    "street",
+                    models.CharField(max_length=100, verbose_name="Street"),
+                ),
+                ("number", models.IntegerField(verbose_name="Number")),
             ],
         ),
         migrations.CreateModel(
-            name='ApartmentsImages',
+            name="ApartmentsImages",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('image', models.ImageField(upload_to=buildings.models.ApartmentsImages.upload_to, verbose_name='Image')),
-                ('apartment', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='images', to='buildings.apartments', verbose_name='Apartment')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "image",
+                    models.ImageField(
+                        upload_to=buildings.models.ApartmentsImages.upload_to,
+                        verbose_name="Image",
+                    ),
+                ),
+                (
+                    "apartment",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="images",
+                        to="buildings.apartments",
+                        verbose_name="Apartment",
+                    ),
+                ),
             ],
         ),
         migrations.AddField(
-            model_name='apartments',
-            name='building',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='apartments', to='buildings.building', verbose_name='Building'),
+            model_name="apartments",
+            name="building",
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="apartments",
+                to="buildings.building",
+                verbose_name="Building",
+            ),
         ),
     ]
--- building/buildings/models.py
+++ building/buildings/models.py
@@ -14,77 +14,117 @@ class Buildings(models.Model):
     street = models.CharField(verbose_name=_("Street"), max_length=100)
     number = models.IntegerField(verbose_name=_("Number"))
 
-    def __str__(self):
-        return self.name or f"г.{self.city} ул.{self.street} д.{self.number}"
-
     class Meta:
         verbose_name = _("Building")
         verbose_name_plural = _("Buildings")
 
+    def __str__(self):
+        return self.name or f"г.{self.city} ул.{self.street} д.{self.number}"
+
 
 class Apartments(models.Model):
     class RoomsChoices(models.TextChoices):
-        ONE = "1", "1",
-        TWO = "2", "2",
-        THREE = "3", "3",
-        FOUR_PLUS = "4+", "4+",
-        STUDIO = "Студия", "Студия",
+        ONE = (
+            "1",
+            "1",
+        )
+        TWO = (
+            "2",
+            "2",
+        )
+        THREE = (
+            "3",
+            "3",
+        )
+        FOUR_PLUS = (
+            "4+",
+            "4+",
+        )
+        STUDIO = (
+            "STUDIO",
+            _("Studio"),
+        )
 
-    area = models.DecimalField(verbose_name=_("Area"), max_digits=5,
-                               decimal_places=2)
-    rooms = models.CharField(verbose_name=_("Rooms"),
-                             choices=RoomsChoices.choices,
-                             default=RoomsChoices.ONE, max_length=20)
-    price = models.DecimalField(verbose_name=_("Price"), max_digits=19,
-                                decimal_places=2)
+    area = models.DecimalField(
+        verbose_name=_("Area"), max_digits=5, decimal_places=2
+    )
+    rooms = models.CharField(
+        verbose_name=_("Rooms"),
+        choices=RoomsChoices.choices,
+        default=RoomsChoices.ONE,
+        max_length=20,
+    )
+    price = models.DecimalField(
+        verbose_name=_("Price"), max_digits=19, decimal_places=2
+    )
     floor = models.PositiveIntegerField(verbose_name=_("Floor"))
-    building = models.ForeignKey(Buildings, related_name="apartments",
-                                 verbose_name=_("Building"),
-                                 on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.building.__str__()} apartment"
+    building = models.ForeignKey(
+        Buildings,
+        related_name="apartments",
+        verbose_name=_("Building"),
+        on_delete=models.CASCADE,
+    )
 
     class Meta:
         verbose_name = _("Apartment")
         verbose_name_plural = _("Apartments")
 
+    def __str__(self):
+        return f"{self.building} apartment"
+
+
+def make_apartment_image_save_path(
+    instance: "ApartmentsImages", filename: str
+) -> str:
+    building = instance.apartment.building
+    address_string = (
+        f"{building.city} {building.street} {building.number}".strip()
+    )
+    return "images/catalog/%s/%s" % (address_string, filename)
+
 
 class ApartmentsImages(models.Model):
-    def upload_to(instance, filename):
-        return 'images/catalog/%s/%s' % (
-            f"{instance.apartment.building.city}"
-            f"{instance.apartment.building.street}"
-            f"{instance.apartment.building.number}",
-            filename)
-
-    apartment = models.ForeignKey(Apartments, related_name="images",
-                                  verbose_name=_("Apartment"),
-                                  on_delete=models.CASCADE)
-    image = models.ImageField(upload_to=upload_to, verbose_name=_("Image"))
-
-    def __str__(self):
-        return f"{self.apartment.__str__()} image"
+    apartment = models.ForeignKey(
+        Apartments,
+        related_name="images",
+        verbose_name=_("Apartment"),
+        on_delete=models.CASCADE,
+    )
+    image = models.ImageField(
+        upload_to=make_apartment_image_save_path, verbose_name=_("Image")
+    )
 
     def delete(self, using=None, keep_parents=False):
-        path = (
-            f"{settings.MEDIA_ROOT}\\{self.image.name}"
-            .replace("/", "\\"))
+        path = f"{settings.MEDIA_ROOT}\\{self.image.name}".replace("/", "\\")
         if os.path.exists(path):
             os.remove(path)
         super().delete(using=using, keep_parents=keep_parents)
 
-    def save(self, force_insert=False, force_update=False, using=None,
-             update_fields=None):
+    def save(
+        self,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+    ):
         path = (
             f"{settings.MEDIA_ROOT}\\"
-            f"{ApartmentsImages.objects.get(pk=self.pk).image.name}"
-            .replace("/", "\\"))
+            f"{ApartmentsImages.objects.get(pk=self.pk).image.name}".replace(
+                "/", "\\"
+            )
+        )
         if os.path.exists(path):
             os.remove(path)
-        super().save(force_insert=False, force_update=force_update, using=using,
-                     update_fields=update_fields)
+        super().save(
+            force_insert=False,
+            force_update=force_update,
+            using=using,
+            update_fields=update_fields,
+        )
 
     class Meta:
         verbose_name = _("Apartment's image")
         verbose_name_plural = _("Apartment's images")
+
+    def __str__(self):
+        return f"{self.apartment} image"
